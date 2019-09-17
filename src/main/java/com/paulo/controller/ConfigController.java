a9598aa84fad4dfc27a62633bc80b47bab734dc9
paulo-t <793489910@qq.com>
2019-09-17 16:02:07 +0800

    1

--- src/main/java/com/paulo/controller/ConfigController.java
+++ src/main/java/com/paulo/controller/ConfigController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConfigController {
     @Autowired
     private UserConfig userConfig;
-    
+
     @GetMapping("/getConfig")
     public UserConfig config(){
         return userConfig;
